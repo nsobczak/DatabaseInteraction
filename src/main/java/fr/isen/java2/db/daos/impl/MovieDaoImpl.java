dd36d5e902e4998ec97ba304e8cbc25e278c448b
nsobczak <nicolas.sobczak@orange.fr>
2017-02-13 20:16:17 +0100

    Refractor listMoviesByGenre

--- src/main/java/fr/isen/java2/db/daos/impl/MovieDaoImpl.java
+++ src/main/java/fr/isen/java2/db/daos/impl/MovieDaoImpl.java
@@ -1,15 +1,10 @@
 package fr.isen.java2.db.daos.impl;
 
-import com.sun.org.apache.bcel.internal.generic.RETURN;
-import fr.isen.java2.db.entities.Genre;
 import fr.isen.java2.db.entities.Movie;
 import fr.isen.java2.db.mapper.ResultMapper;
-import fr.isen.java2.db.mapper.impl.GeneratedKeysMapper;
 import fr.isen.java2.db.mapper.impl.MovieResultMapper;
 import fr.isen.java2.db.util.QueryExecutor;
 
-import java.sql.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,42 +27,11 @@ public class MovieDaoImpl implements fr.isen.java2.db.daos.MovieDao
     @Override
     public List<Movie> listMoviesByGenre(String genreName) throws Exception
     {
-        List<Movie> returnedList = new ArrayList<>();
-
-        try (Connection connection = DataSourceFactory.getDataSource().getConnection())
-        {
-            String sqlQuery = "SELECT * FROM movie JOIN genre " +
-                    "ON movie.genre_id = genre.idgenre WHERE genre.name = ?";
-            try (PreparedStatement statement = connection.prepareStatement(sqlQuery))
-            {
-                statement.setString(1, genreName);
-                ResultSet resultSet = statement.executeQuery();
-                while (resultSet.next())
-                {
-                    //Movie(Integer id, String title, LocalDate releaseDate,
-                    // Genre genre, Integer duration, String director,String summary)
-                    Movie movie = new Movie(resultSet.getInt("idmovie"),
-                            resultSet.getString("title"),
-                            resultSet.getObject("release_date", LocalDate.class), //cast l'objet release_date en local date
-                            new Genre(resultSet.getInt("genre_id"), resultSet.getString("name")),
-                            resultSet.getInt("duration"),
-                            resultSet.getString("director"),
-                            resultSet.getString("summary")
-                    );
-                    returnedList.add(movie);
-                }
-
-            } catch (SQLException e)
-            {
-                e.printStackTrace();
-            }
-
-        } catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-
-        return returnedList;
+        MovieResultMapper movieResultMapper = new MovieResultMapper();
+        String sqlQuery = "SELECT * FROM movie JOIN genre " +
+                "ON movie.genre_id = genre.idgenre WHERE genre.name = ?";
+        QueryExecutor.executeSelectQuery(sqlQuery, movieResultMapper, genreName);
+        return (movieResultMapper.getParsedList());
     }
 
 
--- src/main/java/fr/isen/java2/db/mapper/impl/MovieResultMapper.java
+++ src/main/java/fr/isen/java2/db/mapper/impl/MovieResultMapper.java
@@ -7,6 +7,7 @@ import fr.isen.java2.db.mapper.ResultMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,7 +20,7 @@ public class MovieResultMapper implements ResultMapper<Movie>
 
     public MovieResultMapper()
     {
-        this.movieList = null;
+        this.movieList = new ArrayList<>();
     }
 
 
